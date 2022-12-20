a881a1cc6dc93e0fac7f7cadcbfe05eb3551696f
ArthurHallAvanade <arthur.hall@avanade.com>
2022-12-20 09:31:35 -0400

    moved files, added http server to node

--- ppserver.js
+++ ppserver.js
@@ -1,9 +1,17 @@
 var http = require('http'),
     faye = require('faye');
 
-var server = http.createServer(),
+var fayeServer = http.createServer(),
     bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 
 console.log("it worked");
-bayeux.attach(server);
-server.listen(8000);
+bayeux.attach(fayeServer);
+fayeServer.listen(8000);
+
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+connect()
+    .use(serveStatic(__dirname))
+    .listen(8080, () => console.log('Server running on 8080...'));
