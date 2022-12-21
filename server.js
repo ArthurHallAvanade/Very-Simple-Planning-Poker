6263068fec5f3523ed70118b6c37eb9cec6fe6a1
ArthurHallAvanade <arthur.hall@avanade.com>
2022-12-21 09:13:56 -0400

    Removed commented code

--- server.js
+++ server.js
@@ -1,23 +1,6 @@
 const port = process.env.PORT 
 const fayeport = process.env.PORT 
 
-/* var http = require('http'),
-    faye = require('faye');
-
-var fayeServer = http.createServer(),
-    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
-
-console.log("it worked");
-bayeux.attach(fayeServer);
-fayeServer.listen(fayeport);
-
-
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-connect()
-    .use(serveStatic(__dirname))
-    .listen(port, () => console.log('Server Running.')); */
 
 var express = require('express');
 var app = express();
